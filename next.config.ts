ae2799cfc45b74c51e4411cd4deb898f66d78dc1
pradattaaryal <pradattaaryal2468@gmail.com>
2025-03-01 14:38:46 +0545

    first commit

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['ecommercex.s3.us-east-1.amazonaws.com'],
   },
-<<<<<<< HEAD
-  eslint: {
-=======
     eslint: {
->>>>>>> 5e49714d4a9548b76517914cf37c91a60ec181cf
     ignoreDuringBuilds: true, // Disables ESLint checks during builds
   },
 };
