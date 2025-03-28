2e3334dfc8eecdea72fcfe390fd4a7b62890876c
pradattaaryal <128461960+pradattaaryal@users.noreply.github.com>
2025-03-28 12:24:48 +0545

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['ecommercex.s3.us-east-1.amazonaws.com'],
+    domains: ['ecommercexx.s3.us-east-1.amazonaws.com'],
   },
     eslint: {
     ignoreDuringBuilds: true, // Disables ESLint checks during builds
